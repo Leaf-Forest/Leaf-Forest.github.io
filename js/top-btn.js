c35337a51209ca701997bb4baa959a3ac21737dc
Leaf-Forest <71573713+Leaf-Forest@users.noreply.github.com>
2020-09-23 11:03:24 -0400

    Update top-btn.js

--- js/top-btn.js
+++ js/top-btn.js
@@ -1,19 +1,19 @@
-			//Get the button
-			var topbutton = document.getElementById("topBtn");
+//Get the button
+var topbutton = document.getElementById("topBtn");
 
-			// When the user scrolls down 250px from the top of the document, show the button
-			window.onscroll = function() {scrollFunction()};
+// When the user scrolls down 250px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-			function scrollFunction() {
-  				if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    					topbutton.style.display = "block";
-  				} else {
-    					topbutton.style.display = "none";
-  				}
-			}
+function scrollFunction() {
+  	if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+   		topbutton.style.display = "block";
+ 	} else {
+    		topbutton.style.display = "none";
+  	}
+}
 
-			// When the user clicks on the button, scroll to the top of the document
-			function topFunction() {
-  				document.body.scrollTop = 0;
-  				document.documentElement.scrollTop = 0;
-			}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  	document.body.scrollTop = 0;
+  	document.documentElement.scrollTop = 0;
+}
